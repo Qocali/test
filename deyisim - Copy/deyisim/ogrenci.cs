a8ce506bfd9b8bc4d9de61190c813e7109e3b27c
Qocali <babayevqocheli@gmail.com>
2021-09-20 08:00:54 +0400

    elavelerim

---
+++ deyisim - Copy/deyisim/ogrenci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deyisim
+{
+    class ogrenci
+    {
+        public  string ad;
+        public   string soyad;
+        private string password;
+        public string pass
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+            
+
+    }
+    
+}
---
+++ deyisim - Copy/isnull/isnull/sad.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isnull
+{
+    class sad
+
+    {
+         public virtual  int salam(int saylar)
+        {
+            return saylar;
+
+        }
+       
+    }
+    class over:sad
+    {
+        public override  int salam(int saylar)
+        {
+            return saylar;
+
+        }
+    }
+}
